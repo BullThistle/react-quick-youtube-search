d1eb4a0568d003cbe4c11b44cb568c659f64faed
Rafa <rfurry@gmail.com>
2018-04-08 09:36:52 -0700

    Set up semnatic ui react

---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { List } from 'semantic-ui-react';
+
+const VideoListItem = (props) => {
+  return <List.Item>Video</List.Item>;
+}
+
+export default VideoListItem;
