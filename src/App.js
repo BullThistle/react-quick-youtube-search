7abe7a6687351b31d6945be213ef4fc23b5204bb
Rafa <rfurry@gmail.com>
2018-04-08 08:26:51 -0700

    Make youtube api call with YTSearch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import './App.css';
 
 import SearchBar from './components/search_bar';
 
+console.log(process.env.REACT_APP_YOUTUBE_API);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: process.env.REACT_APP_YOUTUBE_API, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
