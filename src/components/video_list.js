d740a666a1a6203d35396b824f9cd9a572a934f6
Rafa <rfurry@gmail.com>
2018-04-08 19:31:39 -0700

    Create video select

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,9 +3,15 @@ import VideoListItem from './video_list_item';
 import { List } from 'semantic-ui-react';
 
 const VideoList = (props) => {
-const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
-  })
+  const videoItems = props.videos.map( (video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}
+      />
+    );
+  });
   return (
     <List divided verticalAlign='middle'>
       {videoItems}
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
-const VideoList = ({video}) => {
+const VideoList = ({ video, onVideoSelect }) => {
 
   const imageUrl = video.snippet.thumbnails.default.url;
   const title = video.snippet.title;
@@ -13,7 +13,7 @@ const VideoList = ({video}) => {
     <List.Item>
       <Image src={imageUrl} />
       <List.Content style={{ 'position': 'absolute', 'width': '40em' }}>
-        <List.Header as='a' href={url}>{title}</List.Header>
+        <List.Header as='a' onClick={ () => onVideoSelect(video)}>{title}</List.Header>
         <List.Description>{description}</List.Description>
       </List.Content>
     </List.Item>
